e6ac6642c6d182250eb82d954a0c76e932c7007b
Helena Adelsten <helenaltv@gmail.com>
2024-12-18 23:31:50 +0100

    Fix CSS imports and remove TypeScript configuration

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@
 import Footer2 from "./components/underFooter";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import "./styles/globals.css";
+import "./globals.css";
 
 import Hero from "./components/HeroSection";
 import "bootstrap-icons/font/bootstrap-icons.css";
