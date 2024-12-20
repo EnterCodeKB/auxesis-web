3b178eb51896f953ced7cca2f27876e8780678f7
Helena Adelsten <helenaltv@gmail.com>
2024-12-20 15:53:45 +0100

    next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     formats: ["image/avif", "image/webp"], // Aktivera AVIF och WebP
-    domains: ["auxesis.se"], // Externa domäner om nödvändigt
   },
 };
 
