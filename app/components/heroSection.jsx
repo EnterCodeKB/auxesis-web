27b4390731e254d1fdeaf42dc9bdfec26811d580
Helena Adelsten <helenaltv@gmail.com>
2024-12-20 20:01:15 +0100

    heroSection update

--- app/components/heroSection.jsx
+++ app/components/heroSection.jsx
@@ -1,27 +1,29 @@
-import * as React from "react";
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/heroSection.module.css";
+import bgbild from "../../public/produkter/5216B2EC-B52C-4624-8893-85EED9576B09.jpeg";
 import logo from "../../public/Finance/vitlogo.png";
 
-const LanguageSwitcher = () => {
-  return (
-    <div className={styles.languageSwitcher}>
-      <Link href="/">
-        <button>Sv</button>
-      </Link>
-      <Link href="/en">
-        <button>En</button>
-      </Link>
-    </div>
-  );
-};
+const LanguageSwitcher = () => (
+  <div className={styles.languageSwitcher}>
+    <Link href="/">
+      <button>Sv</button>
+    </Link>
+    <Link href="/en">
+      <button>En</button>
+    </Link>
+  </div>
+);
 
 export default function HeroSection() {
   return (
     <div className={styles.heroSection}>
       <div className={styles.div13}>
-        <div className={styles.backgroundImage}></div>
+        <div
+          className={styles.backgroundImage}
+          style={{ backgroundImage: `url(${bgbild.src})` }}
+        ></div>
         <div className={styles.div14}>
           <div className={styles.column}>
             <div className={styles.div15}>
@@ -31,7 +33,7 @@ export default function HeroSection() {
                   alt="Loggan"
                   width={130}
                   height={130}
-                  className="{logo} mr-[1150px] mt-6"
+                  className="mr-[1150px] mt-6"
                 />
               </Link>
               <div className={styles.div16}>
