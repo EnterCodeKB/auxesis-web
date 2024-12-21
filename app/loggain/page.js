57b3d559e60c56e82764dd67a58408c9e16d233e
Helena Adelsten <helenaltv@gmail.com>
2024-12-21 14:10:17 +0100

    update

--- app/loggain/page.js
+++ app/loggain/page.js
@@ -1,5 +1,9 @@
 "use client";
 
+import { useState } from "react"; // Se till att useState importeras korrekt
+import { useRouter } from "next/navigation";
+import "../globals.css"; // Importera globala CSS-stilar
+
 export default function LoginPage() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -13,6 +17,7 @@ export default function LoginPage() {
       setError("Fel lösenord! Försök igen.");
     }
   };
+
   return (
     <div className="login-container">
       <h1 className="login-heading">Sidan är under konstruktion</h1>
