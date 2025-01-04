9a7a8d95cc85ce9cffbbf5a350a3c9c5a9b885db
Helena Adelsten <helenaltv@gmail.com>
2025-01-04 14:55:35 +0100

    footers icon

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -7,19 +7,25 @@ export default function Footer() {
   return (
     <div className={styles.div201}>
       <div className={styles.div202}>
+        <div className={styles.icondivh4}>
+          {" "}
+          <i className={"bi bi-geo-alt"}></i>
+        </div>
         <div className={styles.column}>
-          <h4 className={styles.divh4}>
-            <i className="bi bi-geo-alt"></i>Besöksadress:
-          </h4>
+          <div className={styles.divh4}>
+            <h4 className={styles.h4rubrik}>Besöksadress:</h4>
+          </div>
           <p className={styles.footertext}>Skeppargatan 26</p>
           <p className={styles.footertext}>114 52 Stockholm</p>
           <p className={styles.footertext}>Sweden</p>
         </div>
 
+        <div className={styles.icondivh4}>
+          {" "}
+          <i className="bi bi-envelope"></i>{" "}
+        </div>
         <div className={styles.column}>
-          <h4 className={styles.divh4}>
-            <i className="bi bi-envelope"></i>Postadress:
-          </h4>
+          <h4 className={styles.divh4}>Postadress:</h4>
           <p className={styles.footertext}>P.O.Box 3626</p>
           <p className={styles.footertext}>Regeringsgatan 63</p>
           <p className={styles.footertext}>103 59 Stockholm</p>
