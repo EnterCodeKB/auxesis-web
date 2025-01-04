3b1c33b1f5ffa51ac3e2a05e1f981d0ae01a12f0
Helena Adelsten <helenaltv@gmail.com>
2025-01-04 15:03:09 +0100

    footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-
 import styles from "../styles/Footer.module.css";
 import Link from "next/link";
 
@@ -7,25 +6,21 @@ export default function Footer() {
   return (
     <div className={styles.div201}>
       <div className={styles.div202}>
-        <div className={styles.icondivh4}>
-          {" "}
-          <i className={"bi bi-geo-alt"}></i>
-        </div>
         <div className={styles.column}>
-          <div className={styles.divh4}>
-            <h4 className={styles.h4rubrik}>Besöksadress:</h4>
+          <div className={styles.icondivh4}>
+            <i className="bi bi-geo-alt"></i>
           </div>
+          <h4 className={styles.h4rubrik}>Besöksadress:</h4>
           <p className={styles.footertext}>Skeppargatan 26</p>
           <p className={styles.footertext}>114 52 Stockholm</p>
           <p className={styles.footertext}>Sweden</p>
         </div>
 
-        <div className={styles.icondivh4}>
-          {" "}
-          <i className="bi bi-envelope"></i>{" "}
-        </div>
         <div className={styles.column}>
-          <h4 className={styles.divh4}>Postadress:</h4>
+          <div className={styles.icondivh4}>
+            <i className="bi bi-envelope"></i>
+          </div>
+          <h4 className={styles.h4rubrik}>Postadress:</h4>
           <p className={styles.footertext}>P.O.Box 3626</p>
           <p className={styles.footertext}>Regeringsgatan 63</p>
           <p className={styles.footertext}>103 59 Stockholm</p>
@@ -36,13 +31,17 @@ export default function Footer() {
             +46 (0)8-771 43 00 <br />
             auxesis@auxesispharma.com
           </h4>
-          <img src="/footer/QR.png" width={80} alt="" />
-          &nbsp;&nbsp;&nbsp;&nbsp;
+          <img
+            src="/footer/QR.png"
+            width={80}
+            alt="QR code"
+            className={styles.qrImage}
+          />
           <Link href="https://www.linkedin.com/in/auxesis-pharma-holding-ab-publ-2b354a224">
             <img
               className={styles.imglinkedin}
               src="/linkedinlogo 2.png"
-              alt="linkedin"
+              alt="LinkedIn"
             />
           </Link>
         </div>
