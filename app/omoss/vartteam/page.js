815e7039601b194729e749b5a801d2b5f5956674
Helena Adelsten <helenaltv@gmail.com>
2025-01-16 13:06:54 +0100

    changes

--- app/omoss/vartteam/page.js
+++ app/omoss/vartteam/page.js
@@ -9,10 +9,9 @@ const teamMembers = [
     name: "Roar Adelsten",
     title: "VD, grundare och styrelseledamot",
     description:
-      "Roar Adelsten är grundaren av Auxesis Pharma Holding AB, Auxesis MedTech AB och Auxesis Animal Care AB. <br /><br />" +
-      "Roar har under sin yrkeskarriär arbetat nästan 30 år inom läkemedelsindustrin som VD, Field Manager och medicinsk konsult, bland annat på Pfizer AS i Norge.<br /><br />  " +
-      "Han har erfarenhet av medicinindustrin i Sverige, Norge och USA " +
-      "och har arbetat med kirurgisk rekonstruktiv ortopedi, led- och benartroplastik, kirurgiska instrument med mera. ",
+      "Roar Adelsten grundade Auxesis Pharma Holding AB, MedTech AB och Animal Care AB. <br /><br />" +
+      "Han har nästan 30 års erfarenhet inom läkemedelsindustrin som VD, Field Manager och medicinsk konsult, bland annat på Pfizer AS i Norge. <br /><br />  " +
+      "Roar har arbetat i Sverige, Norge och USA med ortopedi, kirurgiska instrument och led- samt benartroplastik.",
 
     image: "/team/4DF7E618-AFD1-48C9-A7D8-C06E869B29C7.jpeg",
   },
@@ -20,10 +19,9 @@ const teamMembers = [
     name: "Lars Larsson",
     title: "CFO och affärsrådgivare, ekonomi, finansiering och management ",
     description:
-      "Lars Larsson har närmare två decenniers erfarenhet som managementkonsult och arbetar med stöd till företagsledare, vd:ar, styrelseledamöter och företagare inom affärsutveckling och ekonomi.  <br /><br />" +
-      "Han har även en mångårig erfarenhet från olika typer av chefsbefattningar, främst inom IT- och telekombranschen, men har även erfarenhet från ekonomi och finans i fastighetsbranschen. <br /><br />  " +
-      "Inom telekom har han bred erfarenhet som nätdriftschef på Östersunds teleområde till ansvarig global leveranschef för hela Telias CPE-verksamhet, dvs all fysisk utrustning. " +
-      "Med både en BSc i Public Affairs, (fokus på offentlig verksamhet) och en MSc in Management från svenska universitet samt universitetskurser inom högre marknadsföring har Lars en kvalificerad utbildningsbakgrund.",
+      "Lars Larsson har nästan 20 års erfarenhet som managementkonsult och arbetar med affärsutveckling och ekonomi. <br />" +
+      "Han har haft chefsroller inom IT, telekom och fastighetsbranschen, bland annat som global leveranschef på Telia.  <br /><br />  " +
+      "Med en BSc i Public Affairs, MSc i Management och kurser i marknadsföring har han en gedigen utbildningsbakgrund.",
     image: "/team/LarsJpeg.jpeg",
   },
   {
@@ -31,17 +29,17 @@ const teamMembers = [
     title:
       "Styrelseordförande och affärsrådgivare, bolagsutveckling, PR och IR",
     description:
-      "Rune Nordström är arbetande styrelseordförande i Auxesis. Han är en driven entreprenör med lång erfarenhet från bolagsstyrning, ledning, marknadsföring, civilsamhället, politik och kommunikation.  <br /><br />" +
-      "Rune är medgrundare till Thinc som är en börsnoterad koncern på Spotlight stock market. Tidigare har han haft ledande befattningar som direktör på Svenska Kommunförbundet och Landstingsförbundet (nuvarande SKR) och som informationschef på den tidigare statliga myndigheten Arbetslivsinstitutet.",
+      "Rune Nordström är arbetande styrelseordförande i Auxesis och en erfaren entreprenör inom bolagsstyrning, ledning, marknadsföring och kommunikation.<br /><br />" +
+      "Han är medgrundare till börsnoterade Thinc och har haft ledande roller som direktör på SKR:s föregångare och som informationschef på Arbetslivsinstitutet.",
     image: "/team/Rune.jpeg",
   },
   {
     name: "Charlotta Larsson",
     title: "Styrelseledamot och affärsrådgivare, sälj och marknad ",
     description:
-      "Charlotta Larsson är en internationellt erfaren strategiexpert inom marknadsföring av FMCG och OTC produkter, med över 25 års erfarenhet utomlands där hon framgångsrikt har haft ledande befattningar.  <br /><br />" +
-      "Charlotta har arbetat både strategiskt och operativt och har lett team över hela världen för att driva lönsam tillväxt i multinationella konsumentvaror och OTC -företag genom globala varumärkes- och produktportföljstrategier. <br /><br />  " +
-      "Vidare har hon lång erfarenhet från att bygga och driva nystartade företag som backas upp av investerare. Hon har en Master of Science in Business Administration från Handelshögskolan vid Göteborgs Universitet.",
+      "Charlotta Larsson är en strategiexpert inom FMCG och OTC med över 25 års internationell erfarenhet.<br /><br />" +
+      "Hon har lett globala team, utvecklat varumärkesstrategier i multinationella företag och byggt nystartade investerarstödda bolag.  <br /><br />  " +
+      "Charlotta har en MSc i Business Administration från Handelshögskolan i Göteborg.",
     image: "/team/CharlottaJpeg.jpeg",
   },
 
@@ -49,19 +47,18 @@ const teamMembers = [
     name: "Helena Adelsten",
     title: "Webbutvecklare/ Management Support ",
     description:
-      "Helena Adelsten är en mångsidig och professionell med en akademisk bakgrund inom IT och ekonomi, expert inom logistik och systemutveckling.  <br /><br />" +
-      "Som mjukvaruingenjör och webbutvecklare designar hon och utvecklar appar och programvaror. Det innebär att man skapar kod, testar den och implementerar den i olika system eller applikationer.  <br /><br />  " +
-      "Vidare arbetar hon med att lösa problem, förbättra prestanda och säkerställa att programvaran fungerar korrekt och effektivt, skapa helt nya produkter eller tjänster med nya metoder, och hon utvecklar nya algoritmer i avancerade tekniker samt förbättrar användarupplevelsen." +
-      "Helena är också grundare av EnterCode Sweden KB, som bedriver verksamhet inom webbutveckling, webbproduktion och webbprogrammering med fokus på kundanpassade tjänster samt erbjuder teknisk support.",
+      "Helena Adelsten är mjukvaruingenjör och webbutvecklare med en akademisk bakgrund inom IT och ekonomi.  <br /><br />" +
+      "Hon designar och utvecklar appar, löser problem, förbättrar prestanda och utvecklar nya algoritmer. <br /><br />  " +
+      "Som grundare av EnterCode Sweden KB fokuserar hon på webbutveckling, kundanpassade tjänster och teknisk support för att förbättra användarupplevelsen och skapa innovativa lösningar.",
     image: "/team/HelenaJpeg.jpeg",
   },
   {
     name: "Peter Hedberg",
     title: "Immateriella rättigheter, affärsjuridiska avtal & tvistlösning ",
     description:
-      "Peter Hedberg är specialiserad inom immaterialrätt, marknadsföring, affärsjuridik och tvistlösning. Som rådgivare till svenska och internationella företag har han omfattande erfarenhet av överträdelseförfaranden och strategisk IP-rådgivning.  <br /><br />" +
-      "Han har även bistått klienter i en rad fall inom immaterialrätt och andra kommersiella områden. <br />  " +
-      "Peters breda kompetens gör honom till en uppskattad rådgivare i både svenska och internationella sammanhang. ",
+      "Peter Hedberg är expert inom immaterialrätt, marknadsföring, affärsjuridik och tvistlösning.  <br /><br />" +
+      "Han har omfattande erfarenhet av IP-strategier och överträdelseförfaranden, samt bistått klienter i olika kommersiella frågor. <br /> <br />  " +
+      "Med bred kompetens är Peter en uppskattad rådgivare för svenska och internationella företag.",
     image: "/team/Peter-RambergJpeg.jpeg",
   },
   {
@@ -69,11 +66,9 @@ const teamMembers = [
     title:
       "Affärsrådgivare, organisationsutvecklare inom medtech och läkemedel. ",
     description:
-      "Peter ingår i ASA.P läkemedelsutvecklings teamet.<br /><br />" +
-      "Peter Åberg har mer än tjugo års erfarenhet som organisationsutvecklare och ledare. Har utvecklat framgångsrika organisationer och innehaft positioner som VD, marknads- och hållbarhetsdirektör från start-up till börsbolag inom medicinteknik, farmakologi, tillverkningsindustrin och tjänsteföretag.  <br /><br />" +
-      "Inom vården har Peter varit klinisk chef för läkare och administrativ personal. Har varit VD och hållbarhetsdirektör för ett rekryteringsföretag och utsågs två år i rad till Dagens Industris Supergasell som Sveriges snabbast växande företag i alla kategorier.<br /><br />  " +
-      "Peter har en Med kand med toxikolog samt genomgått IFL på Handelshögskolans executive program samt Försvarsmaktens olika ledarprogram.  " +
-      "Dessutom utvecklar han internationella hållbarhetsstandarder, ISO, inom ledarskap, organisation, hållbarhet och är FN-representant i Sverige för hållbar miljö, kvalité och affärsutveckling. ",
+      "Peter Åberg, del av ASA.P:s läkemedelsutvecklingsteam, har över 20 års erfarenhet som ledare och organisationsutvecklare. <br />" +
+      "Han har haft roller som VD och hållbarhetsdirektör inom medicinteknik, farmakologi och tillverkningsindustrin. <br /><br />" +
+      "Peter är Med kand i toxikologi, FN-representant och arbetar med internationella hållbarhetsstandarder för ledarskap, miljö och affärsutveckling.",
     image: "/team/PeterJpeg1.jpeg",
   },
   {
@@ -82,27 +77,25 @@ const teamMembers = [
     description:
       "Susanna ingår i ASA.P läkemedelsutvecklings teamet.<br /><br />" +
       "Över 10 års erfarenhet från AstraZenecas forskningsavdelning  <br /><br />" +
-      "På Zelmic är hon specialiserad på topisk leverans av läkemedel med in-vitro modellering, formuleringsutveckling, hållbarhetsstudier, hudens penetrationsförmåga, valideringsstudier och paketering av läkemedelsdoser till kund med tillhörande etikettsregler ",
+      "på Zelmic är hon specialiserad på topisk leverans av läkemedel med in-vitro modellering, formuleringsutveckling, hållbarhetsstudier, hudens penetrationsförmåga, valideringsstudier och paketering av läkemedelsdoser till kund med tillhörande etikettsregler. ",
     image: "/team/Susanna Lilje.jpeg",
   },
   {
     name: "Agneta Larhed",
     title: "MSci Pharm och PhD.",
     description:
-      "Agneta ingår i ASA.P läkemedelsutvecklings teamet.<br /><br />" +
-      "Agneta har varit verksam som forskare och chef inom både akademisk och industriell läkemedelsinnovation och produktutveckling i mer än 10 år. <br /><br />" +
-      "Hennes arbete fokuserade på läkemedelsabsorption, läkemedelsleverans och formuleringsutveckling, och täckte en rad formuleringar och doseringsformer som tabletter, geler och mikropartiklar. <br /><br />  " +
-      "Dessutom har hon varit delaktig i utvecklingen av kurser och symposier och är idag styrelseledamot i den vetenskapliga sektionen Farmaceutik i Läkemedelsakademien.",
+      "Agneta ingår i ASA.P:s läkemedelsutvecklingsteam och har över 10 års erfarenhet som forskare och chef inom läkemedelsinnovation.<br /><br />" +
+      "Hennes fokus har varit på absorption, leverans och formuleringsutveckling för tabletter, geler och mikropartiklar.  <br /><br />" +
+      "Hon är styrelseledamot i Farmaceutik vid Läkemedelsakademien och har bidragit till kurser och symposier.",
     image: "/team/AgnetaJpeg.jpeg",
   },
   {
     name: "Marie Gårdmark",
     title: "MSci Pharm och PhD.",
     description:
-      "Marie ingår i ASA.P läkemedelsutvecklings teamet.<br /><br />" +
-      "Dr Gårdmark har 10 års erfarenhet från ledande roller i små och stora läkemedelsföretag främst inom regulatorisk FoU, vilket exemplifieras av utveckling och implementering av globala regulatoriska strategier, due diligence-aktiviteter och vetenskapliga rådgivningsmöten i EU och USA. <br /><br />" +
-      "Hennes huvudfokus har varit klinisk och preklinisk forskning med flera års erfarenhet från tidig läkemedelsutveckling.  Hennes vetenskapliga bakgrund är inom klinisk farmakologi och farmakometri.</<br /><br />  " +
-      "Bakgrund inom Läkemedelsverket",
+      "Marie ingår i ASA.P:s läkemedelsutvecklingsteam.<br />" +
+      "Dr Gårdmark har 10 års erfarenhet från ledande roller inom regulatorisk FoU, med fokus på globala strategier, due diligence och vetenskapliga rådgivningsmöten i EU och USA.  <br /><br />" +
+      "Hon har arbetat med klinisk och preklinisk forskning, främst inom tidig läkemedelsutveckling, och har en bakgrund inom klinisk farmakologi och farmakometri.",
     image: "/team/MarieJpeg.jpeg",
   },
 ];
@@ -120,7 +113,7 @@ export default function VartTeam() {
       {/* Infografik Bild */}
       <div className={styles.infografikContainer}>
         <Image
-          src="/team/infografik2.png"
+          src="/team/infografik3.png"
           alt="Infografik"
           width={800}
           height={400}
