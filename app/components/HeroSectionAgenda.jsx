683e5931a82c6db4ff30be7c45eb7604b6825611
Helena Adelsten <helenaltv@gmail.com>
2025-02-02 15:54:39 +0100

    agenda

--- app/components/HeroSectionAgenda.jsx
+++ app/components/HeroSectionAgenda.jsx
@@ -1,23 +1,30 @@
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
 import styles from "../styles/heroAgenda.module.css";
-import AnimatedText2 from "../components/AnimatedText2";
 
 export default function HeroPr() {
   return (
     <div className={styles.heroSection}>
+      {/* Första sektionen med bilden */}
       <div className={styles.div13}>
-        <div className={styles.div14}>
-          <div className={styles.column}>
-            <div className={styles.div15}>
-              <div className={styles.div18}></div>
-            </div>
-          </div>
-          <div className={styles.column2}></div>
-        </div>
+        <img
+          src="/agenda/chema4.png"
+          alt="Ekologisk hållbarhet - hållbar innovation"
+          className={styles.responsiveImage}
+        />
       </div>
-      <div className={styles.div000}></div>
+
+      {/* Text och innehåll bredvid bilden */}
+      <div className={styles.div14}>
+        <div className={styles.column}>
+          <div className={styles.div15}>
+            <div className={styles.div18}></div>
+          </div>
+        </div>
+        <div className={styles.column2}></div>
+      </div>
+
+      {/* Extra sektion (är tom, kan tas bort om ej används) */}
+      {/* <div className={styles.div000}></div> */}
     </div>
   );
 }
